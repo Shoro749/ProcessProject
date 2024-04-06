58304826b4ab946476fd9a7150deed911ce44b65
Shoro749 <romanbro749@gmail.com>
2024-04-06 16:17:13 +0300

    Task completed

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("1) Open calculator\n2) Open notepad\n3) Open paint\nChoice: ");
+        int select = Convert.ToInt32(Console.ReadLine());
+
+        switch (select)
+        {
+            case 1:
+                Process.Start("win32calc.exe");
+                break;
+
+            case 2:
+                Process.Start("notepad.exe");
+                break;
+
+            case 3:
+                Process.Start("mspaint.exe");
+                break;
+        }
+    }
+}
